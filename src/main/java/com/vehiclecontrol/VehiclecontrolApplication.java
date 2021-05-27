1c001effd3fa93f5684b4928de8675a4d2b3514a
Rafael Luiz <rafael.s.luiz@gmail.com>
2021-05-27 19:25:33 -0300

    Project created

---
+++ src/main/java/com/vehiclecontrol/VehiclecontrolApplication.java
@@ -0,0 +1,13 @@
+package com.vehiclecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclecontrolApplication.class, args);
+	}
+
+}
