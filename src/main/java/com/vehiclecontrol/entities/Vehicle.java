4a5cc72cbf32267e3e319ea85e50132109e34ebf
Rafael Luiz <rafael.s.luiz@gmail.com>
2021-05-27 21:41:23 -0300

    Database sedd and Domain Model

--- src/main/java/com/vehiclecontrol/entities/Vehicle.java
+++ src/main/java/com/vehiclecontrol/entities/Vehicle.java
@@ -1,12 +1,26 @@
 package com.vehiclecontrol.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_vehicles")
 public class Vehicle {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String brand;
 	private String model;
 	private Integer year;
 	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
 	private Client client;
 	
 	public Vehicle() {
